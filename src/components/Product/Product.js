a9f71ffe45913dd94c4e50f22c40c6aee94ad334
unknown <kamila.wilczynska92@gmail.com>
2023-01-02 16:12:49 +0000

    add useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import ProductImage from "../ProductImage/ProductImage";
 import ProductForm from "../ProductForm/ProductForm";
+import { useMemo } from "react";
 
 const Product = (props) => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
@@ -10,22 +11,25 @@ const Product = (props) => {
   const [currentSizePrice, setCurrentSizePrice] = useState(
     props.sizes[0].additionalPrice
   );
-  function totalPrice() {
-    return props.basePrice + currentSizePrice;
+  //function totalPrice() {
+    //return props.basePrice + currentSizePrice;
+	function newtotalPrice(a, b) {
+		return a + b;
+
   }
+
+  const totalPrice = useMemo(()=> newtotalPrice(props.basePrice, currentSizePrice), [props.basePrice, currentSizePrice]);
+
+
   const shoppingSummary = {
     name: props.title,
     color: currentColor,
     size: currentSize,
-    price: totalPrice(),
+    //price: totalPrice(),
+	price: totalPrice,
   };
 
-  Product.propTypes = {
-	id: PropTypes.number.isRequired,
-	name: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	basePrice: PropTypes.number.isRequired,
-  };
+
   
   return (
     <article className={styles.product}>
@@ -33,7 +37,8 @@ const Product = (props) => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {totalPrice()}$</span>
+          
+		  <span className={styles.price}>Price: {totalPrice}$</span>
         </header>
         <ProductForm
           colors={props.colors}
@@ -54,4 +59,10 @@ const Product = (props) => {
   );
 };
 
+Product.propTypes = {
+	id: PropTypes.number.isRequired,
+	name: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	basePrice: PropTypes.number.isRequired,
+  };
 export default Product;
